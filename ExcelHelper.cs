40985fd8ae120d1cf7b88b4ea82d1b3e59ba8c93
Corby <chichols@rcp.com>
2021-09-21 11:55:27 -0500

    I added a new ExcelHelper class, so GetColumnLetter is available from ExcelCellAddress.

---
+++ ExcelHelper.cs
@@ -0,0 +1,45 @@
+﻿
+
+#region using statements
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+#endregion
+
+namespace DataJuggler.Excelerate
+{
+
+    #region class ExcelHelper
+    /// <summary>
+    /// method [Enter Method Description]
+    /// </summary>
+    public class ExcelHelper
+    {
+
+        #region Methods
+
+            #region GetColumnLetter(int column)
+            /// <summary>
+            /// returns the Column Letter for the column index (1 = A, 2 = B, 27 = AA, 78 = "ZZZ" I think)
+            /// </summary>
+            public static string GetColumnLetter(int column)
+            {
+                // initial value
+                string columnLetter = ExcelCellAddress.GetColumnLetter(column);
+                
+                // return value
+                return columnLetter;
+            }
+            #endregion
+            
+        #endregion
+
+    }
+    #endregion
+
+}
